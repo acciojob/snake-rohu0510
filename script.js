7cea95ce5d6d545a48c29303b47576ca4b6acdf3
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2023-02-22 22:28:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ createFood();
 
 
 // Move snake:
-var moveSnakeInterval = setInterval(moveSnake, 100);
+var moveSnakeInterval = setInterval(moveSnake, 1000);
 
 
 // Call change direction function on keyboard key-down event:
